f5d3102943b4c8349a4341a7c745a054e5ade3b6
Hao Zhang <HaoZhangZYB@outlook.com>
2020-04-06 17:34:20 -0400

    change default icon

--- public/index.js
+++ public/index.js
@@ -83,7 +83,7 @@ function initBotConversation() {
         domain: domain
     });
     const styleOptions = {
-        botAvatarImage: 'https://docs.microsoft.com/en-us/azure/bot-service/v4sdk/media/logo_bot.svg?view=azure-bot-service-4.0',
+        botAvatarImage: 'https://www.globalvirustracker.org/assets/favicon.ico',
         // botAvatarInitials: '',
         // userAvatarImage: '',
         hideSendBox: true, /* set to true to hide the send box from the view */
